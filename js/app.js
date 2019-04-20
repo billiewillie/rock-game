1f182096c0b5b0e1141a4d6e5ad4a55ca91a183f
Billie Willie <vadimbelinovich@gmail.com>
2019-04-20 13:12:50 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -32,14 +32,14 @@ const game = () => {
   const playMatch = () => {
     options.forEach(option => {
       option.addEventListener("click", function() {
-        playerHand.src = `../img/rock.png`;
-        computerHand.src = `../img/rock.png`;
+        playerHand.src = "../img/rock.png";
+        computerHand.src = "../img/rock.png";
         const computerNumber = Math.floor(Math.random() * 3);
         const computerChoice = computerOptions[computerNumber];
         setTimeout(() => {
           compareHands(this.textContent, computerChoice);
-          playerHand.src = `../img/${this.textContent}.png`;
-          computerHand.src = `../img/${computerChoice}.png`;
+          playerHand.src = "../img/" + this.textContent + ".png";
+          computerHand.src = "../img/" + computerChoice + ".png";
         }, 2000);
         playerHand.style.animation = "shakePlayer 2s ease";
         computerHand.style.animation = "shakeComputer 2s ease";

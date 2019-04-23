abc2c39fd7a566e8c788feec34470cb9fa58d1ce
Billie Willie <vadimbelinovich@gmail.com>
2019-04-24 02:30:48 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -32,8 +32,8 @@ const game = () => {
   const playMatch = () => {
     options.forEach(option => {
       option.addEventListener("click", function() {
-        playerHand.src = `/img/rock.png`;
-        computerHand.src = `/img/rock.png`;
+        playerHand.src = `./img/rock.png`;
+        computerHand.src = `./img/rock.png`;
         const computerNumber = Math.floor(Math.random() * 3);
         const computerChoice = computerOptions[computerNumber];
         playerHand.style.animation = "shakePlayer 2s ease";
